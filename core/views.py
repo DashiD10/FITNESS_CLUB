47a03792bc12ef81283173323a54fd8842cbd40a
Dashi <dorzhiev_dd@mail.ru>
2025-09-02 23:22:19 +1200

    Настроил маршрутизацию файла thanks

--- core/views.py
+++ core/views.py
@@ -12,3 +12,6 @@ def order_detail(request, order_id):
     :param order_id: int (номер заказа)
     """
     return HttpResponse(f"<h1>Детали заказа {order_id}</h1>")
+
+def thanks(request):
+    return render(request, "core/thanks.html")
--- fitness_club/urls.py
+++ fitness_club/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import landing
+from core.views import landing, thanks
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", landing, name="home"),
+    path('', landing, name='home'),
+    path('thanks/', thanks, name='thanks'),
 ]
