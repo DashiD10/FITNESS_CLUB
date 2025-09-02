befce14b4bd79ced300193ba13ffe38a7b910e7a
Dashi <dorzhiev_dd@mail.ru>
2025-09-02 21:23:10 +1200

    Создал приложение core, папку templates, первый шаблон thanks

---
+++ core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+def landing(request):
+    """Обработчик главной страницы"""
+    return HttpResponse("<h1>Главная страница</h1>")
+
+def order_detail(request, order_id):
+    """
+    Отвечает за маршрут 'orders/<int:order_id>/'
+    :param request: HttpRequest
+    :param order_id: int (номер заказа)
+    """
+    return HttpResponse(f"<h1>Детали заказа {order_id}</h1>")
