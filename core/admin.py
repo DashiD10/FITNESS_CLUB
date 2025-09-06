8da36af227178636d422d838def895cba75c195e
Dashi <dorzhiev_dd@mail.ru>
2025-09-06 20:36:34 +1200

    Подключение моделей в административной панели и регистрация в файде admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Service, Trainer, Order, Review
 
 # Register your models here.
+admin.site.register(Service)
+admin.site.register(Trainer)
+admin.site.register(Order)
+admin.site.register(Review)
