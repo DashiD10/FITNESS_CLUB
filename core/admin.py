2e4439eb143ab79fcd3e96ac9ec6354ae236b9e5
Dashi <dorzhiev_dd@mail.ru>
2025-09-15 03:50:54 +1200

    Доработка интерфейса страницы создания тренера

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Service, Trainer, Order, Review
 
-# Register your models here.
+class TrainerAdmin(admin.ModelAdmin):
+    filter_horizontal = ('services',)
+
+    def get_changeform_initial_data(self, request):
+        # Override initial data for add form to set services empty
+        initial = super().get_changeform_initial_data(request)
+        initial['services'] = []
+        return initial
+
 admin.site.register(Service)
-admin.site.register(Trainer)
+admin.site.register(Trainer, TrainerAdmin)
 admin.site.register(Order)
 admin.site.register(Review)
